40f5f2c98e9ff9e4a776724bda30f570f41418f4
kai.wang <kai.wang.john@gmail.com>
2014-04-02 15:16:23 +0800

    api

--- fascinate/urls.py
+++ fascinate/urls.py
@@ -1,14 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 from django.contrib import admin
-from rest_framework import routers
-from fascinate import views
 
 admin.autodiscover()
 
-router = routers.DefaultRouter()
-router.register(r'(?P<img_id>\d+)/$', views.api_post)
-router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = patterns('fascinate.views',
     # url(r'^$', 'django_apps.views.home', name='home'),
@@ -16,9 +11,8 @@ urlpatterns = patterns('fascinate.views',
     url(r'^(?P<img_id>\d+)/$', 'post'),
     url(r'^upload/$', 'upload'),
     # API
-    url(r'^api/', include(router.urls)),
-    # url(r'^api/(?P<img_id>\d+)/$', 'api_post'),
-    # url(r'^api/vote/(?P<img_id>\d+)/$', 'api_post_vote'),
-    # url(r'^api/recent/$', 'api_recent'),
-    # url(r'^api/popular/$', 'api_popular'),
+    url(r'^api/(?P<img_id>\d+)/$', 'api_get_post'),
+    url(r'^api/vote/(?P<img_id>\d+)/$', 'api_get_post_vote'),
+    url(r'^api/recent/$', 'api_recent'),
+    url(r'^api/popular/$', 'api_popular'),
 )
--- fascinate/views.py
+++ fascinate/views.py
@@ -1,17 +1,11 @@
 from django.core import serializers
 
 from django.http.response import HttpResponse
-
 from django.shortcuts import render_to_response
-from rest_framework import viewsets
-from rest_framework.decorators import api_view, parser_classes
-from rest_framework.parsers import JSONParser
-from rest_framework.tests.users.serializers import UserSerializer
 
 from base.ModelToJson import ModelToJson
 
 from fascinate.models import Img, Vote
-from fascinate.serializers import ImgSerializer
 
 
 def index(request):
@@ -26,21 +20,20 @@ def post(request, img_id):
     return render_to_response('fascinate/post.html', {'post': p})
 
 
-@api_view(['POST'])
-def api_post(request, img_id):
+# API
+def api_get_post(request, img_id):
     """
      get img by id(ajax)
     """
-    # p = Img.objects.get(pk=img_id)
-    # return HttpResponse(ModelToJson.to_json(p), content_type="application/json")
-    model = Img.objects.get(pk=img_id)
-    serializer_class = ImgSerializer
+    p = Img.objects.get(pk=img_id)
+    return HttpResponse(ModelToJson.to_json(p), content_type="application/json")
 
 
-# class GetPostById(viewsets.ModelViewSet):
-#     queryset = Img.objects.get(pk=img_id)
-#     serializer_class = UserSerializer
+def api_get_post_vote(request, img_id):
+    """
+    get vote by img id
+    """
+    votes = Vote.objects.filter(img_id=img_id)
+    return HttpResponse(serializers.serialize('json', votes), content_type="application/json")
 
-def ajax_post_vote(request, img_id):
-    vote = Vote.objects.filter(img_id=img_id)
-    return HttpResponse(serializers.serialize('json', vote), content_type="application/json")
+
